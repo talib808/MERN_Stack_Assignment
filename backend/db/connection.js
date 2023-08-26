d3e6f46999b639bc1daa545579ac1c5ef9470cda
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:40:06 +0530

    Update the code with error code & error handling

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,12 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/Lahagora_assignment', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+// Function to connect to the MongoDB database
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/Lahagora_assignment", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
 
-module.exports = mongoose.connection;
+// Function to close the MongoDB database connection
+const closeDatabaseConnection = async () => {
+  try {
+    await mongoose.connection.close();
+    console.log("Disconnected from MongoDB");
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB:", error);
+  }
+};
+
+
+module.exports = {
+  connectToDatabase,
+  closeDatabaseConnection,
+};
