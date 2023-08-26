1a4270c20f20072909eb8b0c3ce226aa680d56d0
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:47:59 +0530

    Update the code with error code & error handling

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
+// Define the schema for a user
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  phone: String,
-  country: String,
+  name: String,                
+  email: { type: String, unique: true }, 
+  password: String,              
+  phone: String,                 
+  country: String,               
   cart: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Cart',
+    ref: 'Cart',                  // Reference to the user's cart
   },
-  tokens: [],
+  tokens: [],                    // Array to store authentication tokens
 });
 
 module.exports = mongoose.model('User', userSchema);
+
 
