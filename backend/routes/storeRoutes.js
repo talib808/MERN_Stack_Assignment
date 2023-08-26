46c8a1b6e1bc8977ac044d9a4a3455069c7d349f
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:52:07 +0530

    Update the code with error code & error handling

--- backend/routes/storeRoutes.js
+++ backend/routes/storeRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const Store = require('../models/Store');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const authMiddleware = require('../middlewares/auth');
+const express = require("express");
+const Store = require("../models/Store");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const authenticateUser = require("../middlewares/auth");
 
 const router = express.Router();
 
 // Store registration
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     const { name, email, password, phone, storeName } = req.body;
 
     const existingStore = await Store.findOne({ email });
     if (existingStore) {
-      return res.status(400).send({ error: 'Email already exists.' });
+      return res.status(400).send({ error: "Email already exists." });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -30,35 +30,37 @@ router.post('/register', async (req, res) => {
 
     res.status(201).send(newStore);
   } catch (error) {
-    res.status(400).send(error);
+    console.error("Error registering store:", error);
+    res
+      .status(400)
+      .send({ error: "An error occurred while registering the store." });
   }
 });
 
 // Store login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const store = await Store.findOne({ email });
     if (!store) {
-      return res.status(401).send({ error: 'Invalid credentials.' });
+      return res.status(401).send({ error: "Invalid credentials." });
     }
 
     const isPasswordValid = await bcrypt.compare(password, store.password);
     if (!isPasswordValid) {
-      return res.status(401).send({ error: 'Invalid credentials.' });
+      return res.status(401).send({ error: "Invalid credentials." });
     }
 
-    const token = jwt.sign({ _id: store._id }, 'jwtSecretKey');
+    const token = jwt.sign({ _id: store._id }, "jwtSecretKey");
     store.tokens = store.tokens.concat({ token });
     await store.save();
 
-    res.send({ store, token });
+    res.send({ message: "Login successful.", store, token });
   } catch (error) {
-    res.status(400).send(error);
+    console.error("Error logging in:", error);
+    res.status(400).send({ error: "An error occurred while logging in." });
   }
 });
 
-
 module.exports = router;
-
