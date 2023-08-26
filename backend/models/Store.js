40e06eb94c966eb329ca13781f3cd8d99afb63c0
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:42:57 +0530

    Update the code with error code & error handling

--- backend/models/Store.js
+++ backend/models/Store.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+// Define the schema for a store
 const storeSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  phone: String,
-  storeName: String,
-  tokens: [],
+  name: String,                  
+  email: { type: String, unique: true },  
+  password: String,              
+  phone: String,                 
+  storeName: String,             
+  tokens: [],                    // Array to store authentication tokens
 });
 
-module.exports = mongoose.model('Store', storeSchema);
+
+module.exports = mongoose.model("Store", storeSchema);
