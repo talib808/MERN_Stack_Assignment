c259e7eda2084f7b6c48392510008eb22e24b64c
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:39:13 +0530

    Update the code with error code & error handling

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,11 @@
+// Configuration file exporting a JSON Web Token (JWT) secret key
 module.exports = {
+  // The JWT secret key used to sign and verify tokens
   jwtSecretKey: "talibworkhardotogetsuccess123",
 };
+
+
+// This property holds a secret key that will be used for encoding and decoding JSON Web Tokens (JWTs). 
+// JWTs are commonly used for implementing user authentication and authorization mechanisms in web applications.
+// The secret key should be kept confidential and is used to ensure the integrity. 
+// Security of the tokens exchanged between the server and the client
