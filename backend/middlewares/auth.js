a9ef8c9cdd5ac6e874ab2c8640bd337b0d39ef04
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:40:56 +0530

    Update the code with error code & error handling

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,24 +1,40 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const { jwtSecretKey } = require('../config/keys'); 
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { jwtSecretKey } = require("../config/keys");
 
-const authMiddleware = async (req, res, next) => {
+// Define a constant for the token prefix
+const TOKEN_PREFIX = "Bearer ";
+
+// Middleware to authenticate the user
+const authenticateUser = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', ''); // Corrected this line
-    const decoded = jwt.verify(token, jwtSecretKey); 
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+    // Get the token from the authorization header and remove the prefix
+    const token = req.header("Authorization").replace(TOKEN_PREFIX, "");
+    const decoded = jwt.verify(token, jwtSecretKey);
 
+    // Find the user with the decoded ID and matching token
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    // If user not found, send an error response
     if (!user) {
-      throw new Error();
+      return res.status(401).send({ error: "User not found or token invalid." });
     }
 
+    // Attach the token and user information to the request
     req.token = token;
     req.user = user;
+
+    // Proceed to the next middleware or route
     next();
   } catch (error) {
-    res.status(401).send({ error: 'Please authenticate.' });
+    // If token validation fails, send an error response
+    res.status(401).send({ error: "Please authenticate." });
   }
 };
 
-module.exports = authMiddleware;
+
+module.exports = authenticateUser;
 
