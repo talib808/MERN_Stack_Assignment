91ac9c614a16e20645b740bee3692efb8fc32c2c
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:41:46 +0530

    Update the code with error code & error handling

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+// Define the schema for individual items in the cart
 const cartItemSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
+    ref: "Product", // Reference to the 'Product' model
   },
   quantity: Number,
 });
 
+// Define the schema for the user's cart
 const cartSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    ref: "User", // Reference to the 'User' model
   },
-  items: [cartItemSchema],
+  items: [cartItemSchema], // An array of cart items
 });
 
-module.exports = mongoose.model('Cart', cartSchema);
+module.exports = mongoose.model("Cart", cartSchema);
