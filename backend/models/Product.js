d252da665430b714327e6f1cb3c492473212e2c7
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-26 16:42:25 +0530

    Update the code with error code & error handling

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
+// Define the schema for a product
 const productSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String,
-  price: Number,
-  quantity: Number,
+  name: String,         
+  image: String,        
+  description: String,  
+  price: Number,        
+  quantity: Number,    
   store: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Store',
+    ref: 'Store',       // Reference to the 'Store' model
   },
 });
 
+
 module.exports = mongoose.model('Product', productSchema);
+
